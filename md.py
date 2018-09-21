2a868dae76181a7cc8ab7cfb34bcd85e342b056d
dormrod <david.ormrodmorley@balliol.ox.ac.uk>
2018-09-21 08:13:54 +0100

    Add main function and make python 2 compatable

--- md.py
+++ md.py
@@ -4,6 +4,7 @@ Single Molecule Molecular Dynamics Code
 Created 2018 by David of Theoretically Speaking
 Please Modify!
 """
+from __future__ import print_function
 import os
 import numpy as np
 
@@ -18,4 +19,13 @@ bohr = 0.52917721067       # Angstroms
 hbar = 6.626070040e-34     # Js
 atomic_time = hbar/hartree
 
+
+def main():
+    """Handle input/output and molecular dynamics velocity-verlet algorithm"""
+
+    print("hello")
+
+# Execute code if main file
+if __name__ == "__main__":
+    main()
 
