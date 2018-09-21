4f2db09188e7370dc697d9eaf9e99cbd096f5f02
dormrod <david.ormrodmorley@balliol.ox.ac.uk>
2018-09-21 08:07:20 +0100

    Add unit conversions

---
+++ md.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Single Molecule Molecular Dynamics Code 
+Created 2018 by David of Theoretically Speaking
+Please Modify!
+"""
+import os
+import numpy as np
+
+# Global variables for unit conversions
+hartree = 4.35974465e-18   # J, atomic unit of energy
+emass = 5.486e-4           # kg
+dalton = 1.660539040e-27   # kg
+avo = 6.02214086e23        # mol^-1
+emass = 9.109534e-28       # g, atomic unit of mass
+boltz = 1.38064852e-23 / hartree    # E_h K^-1
+bohr = 0.52917721067       # Angstroms
+hbar = 6.626070040e-34     # Js
+atomic_time = hbar/hartree
+
+
